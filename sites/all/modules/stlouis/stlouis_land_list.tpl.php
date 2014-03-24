e127081b6d28f3b95ad85074b35faf543af0f940
Joseph Cheek <joseph@cheek.com>
2014-03-24 14:20:03 -0400

    [#19] remove abc123 special case

--- sites/all/modules/stlouis/stlouis_land_list.tpl.php
+++ sites/all/modules/stlouis/stlouis_land_list.tpl.php
@@ -14,11 +14,11 @@
 
   $sql = 'update users set income =
     (SELECT sum(land.payout * land_ownership.quantity)
-    as income from land 
+    as income from land
     left join land_ownership
     on land_ownership.fkey_land_id = land.id and
     land_ownership.fkey_users_id = %d)
-    where id = %d;';  
+    where id = %d;';
   $result = db_query($sql, $game_user->id, $game_user->id);
 
   $game_user = $fetch_user();
@@ -45,11 +45,11 @@ EOF;
   }
 
   echo <<< EOF
-</div>  
+</div>
 EOF;
 
   if ($game_user->level < 15) {
-    
+
     echo <<< EOF
 <ul>
   <li>Purchase $land_plural_lower to earn hourly income</li>
@@ -57,35 +57,39 @@ EOF;
 EOF;
 
   }
-  
+
   echo <<< EOF
 <div class="title">
 Purchase $land_plural
 </div>
 EOF;
 
-  $sql_to_add = '    WHERE ((
+  $sql_to_add = '
+    WHERE (((
       fkey_neighborhoods_id = 0
       OR fkey_neighborhoods_id = %d
-    ) 
-    
+    )
+
     AND
-    
+
     (
       fkey_values_id = 0
       OR fkey_values_id = %d
     ))
-  
-    AND required_level <= %d
-    AND active =1';
 
-  if ($game_user->phone_id == 'abc123')
-    $sql_to_add = '';
-    
+      AND required_level <= %d
+      AND active = 1
+    )
+
+--    OR land_ownership.quantity > 0 ';
+
+//   if ($game_user->phone_id == 'abc123')
+//     $sql_to_add = '';
+
   $data = array();
   $sql = 'SELECT land.*, land_ownership.quantity
     FROM land
-    
+
     LEFT OUTER JOIN land_ownership ON land_ownership.fkey_land_id = land.id
     AND land_ownership.fkey_users_id = %d
     ' . $sql_to_add . '
@@ -94,19 +98,19 @@ EOF;
     $game_user->fkey_values_id, $game_user->level);
 
   while ($item = db_fetch_object($result)) $data[] = $item;
-  
+
   foreach ($data as $item) {
 firep($item);
-    
+
     $description = str_replace('%clan', "<em>$clan_title</em>",
       $item->description);
-      
+
     $quantity = $item->quantity;
     if (empty($quantity)) $quantity = '<em>None</em>';
 
     $land_price = $item->price + ($item->quantity *
       $item->price_increase);
-      
+
     $ai_output .= " $item->id=$land_price";
 
     if (($land_price % 1000) == 0)
@@ -142,45 +146,45 @@ EOF;
 
   if (substr($phone_id, 0, 3) == 'ai-')
     echo "<!--\n<ai \"$ai_output\"/>\n-->";
-  
+
 // show next one
   $sql = 'SELECT land.*, land_ownership.quantity
     FROM land
-    
+
     LEFT OUTER JOIN land_ownership ON land_ownership.fkey_land_id = land.id
     AND land_ownership.fkey_users_id = %d
 
     WHERE ((
       fkey_neighborhoods_id = 0
       OR fkey_neighborhoods_id = %d
-    ) 
-    
+    )
+
     AND
-    
+
     (
       fkey_values_id = 0
       OR fkey_values_id = %d
     ))
-  
+
     AND required_level > %d
-    AND active =1
+    AND active = 1
     ORDER BY required_level ASC LIMIT 1';
   $result = db_query($sql, $game_user->id, $game_user->fkey_neighborhoods_id,
     $game_user->fkey_values_id, $game_user->level);
 
   $item = db_fetch_object($result);
 firep($item);
-    
+
   if (!empty($item)) {
     $description = str_replace('%clan', "<em>$clan_title</em>",
       $item->description);
-      
+
     $quantity = $item->quantity;
     if (empty($quantity)) $quantity = '<em>None</em>';
-  
+
     $land_price = $item->price + ($item->quantity *
       $item->price_increase);
-      
+
     echo <<< EOF
 <div class="land-soon">
   <div class="land-details">
@@ -195,5 +199,5 @@ firep($item);
 EOF;
 
   }
-  
+
   db_set_active('default');
