a61232991224c8f6b6c372d07420427c5fd3a082
Ashwani Sharma <renuashwani81@gmail.com>
2015-02-27 08:25:46 +0530

    CG-53

--- sites/all/modules/celestial_glory/celestial_glory_error.tpl.php
+++ sites/all/modules/celestial_glory/celestial_glory_error.tpl.php
@@ -21,5 +21,35 @@
   Please report this to <strong>support@cheek.com</strong>.
  
 EOF;
-
+  echo <<< EOF
+echo <<< EOF
+<div class="title">
+<img src="/sites/default/files/images/{$game}_title.png"/>
+</div>
+<p>&nbsp;</p>
+<div class="welcome">
+  <div class="wise_old_man_large">
+  </div>
+  <p>A wizened old man comes up to you.&nbsp; You recognize him as one of the
+    elders of the city.</p>
+  <p class="second">I've been watching you for some time,
+    and I like what I see.&nbsp; I think you have the potential for
+    greatness.&nbsp; Maybe you could even lead this city.</p>
+  <p class="second">Could you?</p>
+  <div class="subtitle">
+    How to play
+  </div>
+  <ul>
+    <li>Finish missions to earn skills and influence</li>
+    <li>Cooperate and compete with other players to achieve your goals</li>
+    <li>Purchase equipment and businesses to win votes</li>
+    <li>Become a city elder, political party leader, and then mayor</li>
+  </ul>
+</div>
+<div class="subtitle">
+  <a href="/$game/quests/$arg2">
+    <img src="/sites/default/files/images/{$game}_continue.png"/>
+  </a>
+</div>
+EOF;
   db_set_active('default');
