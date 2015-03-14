1a9bf5d8854af42851ea0fb740094dab415d6673
Ashwani Sharma <renuashwani81@gmail.com>
2015-03-14 12:39:45 +0530

    update on indentation of error code

--- sites/all/modules/celestial_glory/celestial_glory_error.tpl.php
+++ sites/all/modules/celestial_glory/celestial_glory_error.tpl.php
@@ -1,8 +1,6 @@
 <?php
 
   global $game, $phone_id;
-// we won't have gone through fetch_user() yet, so set these here
-   $game = check_plain(arg(0));
   $get_phoneid = '_' . $game . '_get_phoneid';
   $phone_id = $get_phoneid();
   $arg2 = check_plain(arg(2));
@@ -11,24 +9,24 @@
 
   
 echo <<< EOF
-<div class="title">
-  <img src="/sites/default/files/images/{$game}_title.png"/>
-</div>
-<p>&nbsp;</p>
-<div class="welcome">
-  <div class="wise_old_man_large">
+    <div class="title">
+      <img src="/sites/default/files/images/{$game}_title.png"/>
+    </div>
+    <p>&nbsp;</p>
+    <div class="welcome">
+     <div class="wise_old_man_large">
 	
-  </div>
-  <p>And it came to pass that</p>
-  <p>Error E-{$error_code}</p>
-  <p>happened for user</p>
-  <p> {$phone_id}.</p>
-  <p> Please report this to <strong>support@cheek.com</strong>.</p>
-</div>
-<div class="subtitle">
-  <a href="/$game/quests/$arg2">
-    <img src="/sites/default/files/images/{$game}_continue.png"/>
-  </a>
-</div>
+    </div>
+    <p>And it came to pass that</p>
+    <p>Error E-{$error_code}</p>
+    <p>happened for user</p>
+    <p> {$phone_id}.</p>
+    <p> Please report this to <strong>support@cheek.com</strong>.</p>
+   </div>
+   <div class="subtitle">
+    <a href="/$game/quests/$arg2">
+        <img src="/sites/default/files/images/{$game}_continue.png"/>
+    </a>
+   </div>
 EOF;
-  db_set_active('default');
+    db_set_active('default');
