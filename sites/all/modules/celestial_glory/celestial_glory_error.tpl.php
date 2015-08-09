a7be84299cee3090cfda9d1ce4e15b570a2b8902
Joseph Cheek <joseph@cheek.com.co>
2015-08-09 09:11:51 -0400

    fix error page

--- sites/all/modules/celestial_glory/celestial_glory_error.tpl.php
+++ sites/all/modules/celestial_glory/celestial_glory_error.tpl.php
@@ -1,13 +1,13 @@
 <?php
 
-  global $game, $phone_id;
+  $game = check_plain(arg(0));
   $get_phoneid = '_' . $game . '_get_phoneid';
   $phone_id = $get_phoneid();
   $arg2 = check_plain(arg(2));
 
   db_set_active('game_' . $game);
 
-  
+
 echo <<< EOF
     <div class="title">
       <img src="/sites/default/files/images/{$game}_title.png"/>
@@ -15,7 +15,7 @@ echo <<< EOF
     <p>&nbsp;</p>
     <div class="welcome">
      <div class="wise_old_man_large">
-	
+
     </div>
     <p>And it came to pass that</p>
     <p>Error E-{$error_code}</p>
