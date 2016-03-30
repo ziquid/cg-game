34c488ebaeb8fae6d8189d95c0a58bd8dedc1a3b
Joseph Cheek <joseph@gdgwebworks.com>
2016-03-30 13:42:59 -0400

    h3 for upcoming event titles

--- sites/all/modules/celestial_glory/celestial_glory_homepage.tpl.php
+++ sites/all/modules/celestial_glory/celestial_glory_homepage.tpl.php
@@ -261,13 +261,13 @@ if (($today == '2012-12-26') || $game_user->username == 'abc123')
           <div class="event-title">
             Upcoming Events
           </div>
-          <h4>
+          <h3>
             April 1: All Quests cost 100 Energy
-          </h4>
+          </h3>
           Pick your favorite Quests and get ready to do them!
-          <h4>
+          <h3>
             April 6: Merchant Quests!!!
-          </h4>
+          </h3>
           ~ New Quests and Loot at the bottom of the map ~
           <br>
           ~ Become a Merchant or get your Merchant Comprehension ~
