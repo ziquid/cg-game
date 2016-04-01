32ddaedb1157a19e770e4711d1141f517b45c689
Joseph Cheek <joseph@gdgwebworks.com>
2016-04-01 09:31:15 -0400

    quests_100 notification

--- sites/all/modules/celestial_glory/celestial_glory_homepage.tpl.php
+++ sites/all/modules/celestial_glory/celestial_glory_homepage.tpl.php
@@ -275,7 +275,7 @@ if (($today == '2012-12-26') || $game_user->username == 'abc123')
 
       break;
 
-    case EVENT_APR1:
+    case EVENT_QUESTS_100:
 
       $event_text = '<div class="event">
           <div class="event-title">
